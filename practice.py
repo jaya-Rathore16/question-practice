ec92a2ece86e2a84f40b67683daab238b905756b
Jaya Rathore <rathorejaya@gmail.com>
2026-09-23 00:33:35 +0530

    second question

--- practice.py
+++ practice.py
@@ -3,3 +3,9 @@
 a=10
 print(type(a))
 print(complex(a))
+
+#calcluate SI value
+aum=int(input("Enter Principle Amount"))
+rate=int(input("Enter the rate of intrest per anum"))
+time=int(input("Enter The Time in year"))
+print((aum*rate*time)/100,"is your SI")
